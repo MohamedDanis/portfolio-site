9347022f92f368c41d01aa8313e027384ca9b0e0
Mohamed Danis <62685219+MohamedDanis@users.noreply.github.com>
2023-02-20 13:05:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div className='w-full h-screen flex items-center justify-center'>
+      <h1 className='text-5xl font-Tanohe font-extrabold'>Website Under Construction</h1>
+    </div>
+  )
+}
+
+export default App
